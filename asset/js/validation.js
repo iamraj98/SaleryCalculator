51b6407d8ad879280f331f937804c84a08c8a856
Raj <rajlohar248@gmail.com>
2023-01-08 12:13:04 +0530

    update

--- asset/js/validation.js
+++ asset/js/validation.js
@@ -11,7 +11,7 @@ function validation(){
   }
   else if (u_name == 'admin'){
     if(u_password == 'admin@1998'){
-      window.open('/salery.html','_self');
+      window.open('./salery.html','_self');
     }
     else{
       alert("Password Does not match");
